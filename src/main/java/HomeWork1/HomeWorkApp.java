dfc24c5e574776dffefa2a63aca7b2ff7c815f6c
PavelReshetov1 <105560099+PavelReshetov1@users.noreply.github.com>
2022-05-15 18:55:31 +0300

    Первая домашняя работа Java ровень1

--- src/main/java/HomeWork1/HomeWorkApp.java
+++ src/main/java/HomeWork1/HomeWorkApp.java
@@ -1,2 +1,46 @@
-package HomeWork1;public class HomeWorkApp {
+package HomeWork1;
+// Первая домашняя работа
+public class HomeWorkApp {
+
+    private static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+    private static void checkSumSign() {
+       int a = -512;
+       int b = -256;
+       if (a + b >= 0) {
+           System.out.println("Сумма положительная");
+       } else System.out.println("Сумма отрицательная");
+    }
+    private static void printColor() {
+        int value = 121;
+        if (value <= 0) {
+            System.out.println("Красный");
+        }
+        if (value > 0 && value <=100) {
+            System.out.println("Желтый");
+        }
+        if (value > 100) {
+            System.out.println("Зеленый");
+        }
+    }
+    private static void compareNumbers() {
+        int a = 1024;
+        int b = 1080;
+        if (a >= b) {
+            System.out.println("a>=b");
+        } else {
+            System.out.println("a<b");
+        }
+    }
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+
+    }
+
 }
